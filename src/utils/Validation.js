524d3141ac1bc2656c4daaf8767f2baff2f5f94c
semnil5202 <semnil5202@gmail.com>
2022-12-26 03:08:21 +0900

    feat: 못 먹는 음식이 SAMPLE에 포함되어 있는지 확인하는 예외 처리 기능 추가
    #private 메서드를 활용하기에 class 싱글톤 패턴으로 변경한다.

--- src/utils/Validation.js
+++ src/utils/Validation.js
@@ -1,24 +1,43 @@
-const { COACH, ERROR } = require('./constant');
+const { UTIL, SAMPLE, COACH, ERROR } = require('./constant');
 
-const Validation = {
-  coachName(names) {
-    names.forEach((name) => {
+class Validation {
+  static #instance;
+
+  constructor() {
+    if (Validation.#instance !== undefined) return Validation.#instance;
+    Validation.#instance = this;
+  }
+
+  coach(coachs) {
+    coachs.forEach((name) => {
       if (name.length < COACH.NAME_MIN || name.length > COACH.NAME_MAX) {
         throw new Error(ERROR.COACH_NAME);
       }
     });
-  },
-
-  coachNumber(coach) {
-    if (coach.length < COACH.GROUP_MIN || coach.length > COACH.GROUP_MAX) {
+    if (coachs.length < COACH.GROUP_MIN || coachs.length > COACH.GROUP_MAX) {
       throw new Error(ERROR.GROUP);
     }
-  },
+  }
 
   notEat(input) {
-    const foods = input.split(',');
+    const foods = input.split(UTIL.SEPARATOR);
     if (foods.length > COACH.NOT_EAT_MAX) throw new Error(ERROR.NOT_EAT);
-  },
-};
+    if (!this.#foodsString(foods)) throw new Error('E');
+  }
+
+  #foodsString(foods) {
+    for (let food of foods) {
+      if (!this.#checkSample(food)) return false;
+    }
+    return true;
+  }
+
+  #checkSample(foods) {
+    for (let category in SAMPLE) {
+      if (SAMPLE[category].split(UTIL.FOOD_SEPARATOR).includes(foods)) return true;
+    }
+    return false;
+  }
+}
 
 module.exports = Validation;
